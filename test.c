dd9fd8979055336cc64ee189a0031f59733d4653
Mohith8861 <96484311+Mohith8861@users.noreply.github.com>
2023-05-26 10:55:08 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,11 @@
+//
+// Created by Lenovo on 7/28/2022.
+//
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i<=50; i++)
+        {
+            printf("%d %d %d %d \n",i,i,i,i);
+        }
+}
